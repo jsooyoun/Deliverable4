64d76ba0bd13d72af3123fa5cc844124ba9c7e5a
jsooyoun <113483588+jsooyoun@users.noreply.github.com>
2024-11-10 23:29:20 -0500

    Update java.js

--- java.js
+++ java.js
@@ -1,8 +1,12 @@
+/*will run when document is loadad*/
 document.addEventListener('DOMContentLoaded', function() {
     const sections = document.querySelectorAll('section');
+    /*selects all "section ids*/
 
     function checkVisibility() {
         const triggerBottom = window.innerHeight / 5 * 4;
+
+            /*defines when a section should fade in*/
 
         sections.forEach(section => {
             const sectionTop = section.getBoundingClientRect().top;
@@ -13,12 +17,16 @@ document.addEventListener('DOMContentLoaded', function() {
                 section.classList.remove('visible');
             }
         });
+
+        /*for each section, if the top of the section is less than the fade-in boundary,
+        it is visible, and the section.visible css is applied */
     }
 
     checkVisibility(); 
     window.addEventListener('scroll', checkVisibility); 
 });
 
+/*class JS, that replaces images that do not have a corresponding file with the default image*/
 document.querySelectorAll('img').forEach(img => {
     img.onerror = function() { 
       this.onerror = null; 
@@ -28,17 +36,21 @@ document.querySelectorAll('img').forEach(img => {
   });
 
 
+/*defines a function that occurs whenever the "toggleGallery" gets clicked*/
 document.getElementById('toggleGallery').addEventListener('click', function() {
     var imageContainer = document.getElementById('imageContainer');
     imageContainer.classList.toggle('show-images');
+    /*toggles if the images in imageContainer (the gallery) are showed or not*/
     if (imageContainer.classList.contains('show-images')) {
         this.textContent = 'Hide Gallery';
     } else {
         this.textContent = 'Show Gallery';
     }
+    /*determines the text in the gallery button*/
 });
 
 
+/*if the "hamburger menu" gets clicked, the "activeness" (presence) of the navigation links is determined*/
 const hamburgerMenu = document.getElementById("hamburger-menu");
 const navLinks = document.getElementById("nav-links");
 
